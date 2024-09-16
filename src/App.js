2b6a474da6ccda0f0138e67fea89a4a7d31b002c
青博蓝 <99180192+Qingbolan@users.noreply.github.com>
2024-09-16 14:15:49 +0800

    v.init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ResumeWebsite from './ResumeWebsite';
+
+function App() {
+  return (
+    <div className="App">
+      <ResumeWebsite />
+    </div>
+  );
+}
+
+export default App;
